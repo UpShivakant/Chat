9b6648ce95611ca0deba0c626b3ac4a581d64abe
shivakant <shivakant.upadhyay@timesinternet.in>
2023-04-03 04:20:00 +0530

    extension file added

---
+++ PinkChat/Utils/Color+Extention.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Extention.swift
+//  PinkChat
+//
+//  Created by Shivakant Upadhyay on 03/04/23.
+//
+
+import UIKit
+import Foundation
+
+extension UIColor {
+    // main theme color
+    static let PrimaryPink: UIColor = UIColor(named: "PrimaryPink") ?? #colorLiteral(red: 1, green: 0.2, blue: 0.6666666667, alpha: 1)
+    
+    // second main theme color
+    
+    
+    
+}
