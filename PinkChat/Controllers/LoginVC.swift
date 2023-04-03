bc20ce883b3f250bbd17baf741c269393417ea0b
shivakant <shivakant.upadhyay@timesinternet.in>
2023-04-03 12:47:00 +0530

    test

--- PinkChat/Controllers/LoginVC.swift
+++ PinkChat/Controllers/LoginVC.swift
@@ -11,6 +11,8 @@ class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Do any additional setup after loading the view.
     }
